33975bc4f6443a89329be92b4c0972b969892f4a
MarianaTovar05 <maritovarmoreno02@gmail.com>
2025-05-09 10:30:53 -0500

    Taller

--- compuestos/compu1.java
+++ compuestos/compu1.java
@@ -1,8 +1,7 @@
-/*
-Mayor o menor de edad: solicita la edad e indique si la persona es mayor o menor de edad.
- */
+/*Mayor o menor de edad: solicita la edad e indique si la persona es 
+mayor o menor de edad.*/
 import java.util.Scanner; 
-public class main {
+public class compu1 {
  public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in); 
     System.out.println("solicita la edad");
